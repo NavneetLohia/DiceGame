bd4626437f6f8c9e76f015db3e7059c506a33f2c
NAVNEET LOHIA <mannunavneet31.nl@gmail.com>
2021-01-15 00:34:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.ceil(Math.random() * 6);
+var final1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", final1);
+
+var randomNumber2 = Math.ceil(Math.random() * 6);
+var final2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", final2);
+
+if(final1 === final2) {
+  document.querySelector("h1").innerText = "Draw";
+}
+else if(final1 > final2) {
+  document.querySelector("h1").innerText = "🎉 Player 1 Wins!";
+}
+else {
+  document.querySelector("h1").innerText = "Player 2 Wins! 🎉";
+}
